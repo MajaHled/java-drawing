2f83151b9e92efce62426cc1995640a4c94c7b0d
Marie Hledikova <hledikovam@gmail.com>
2024-09-11 01:22:09 +0200

    Add file operations

--- src/main/java/cz/cuni/mff/java/hw/drawing/DrawPanel.java
+++ src/main/java/cz/cuni/mff/java/hw/drawing/DrawPanel.java
@@ -1,26 +1,27 @@
 package cz.cuni.mff.java.hw.drawing;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class DrawPanel extends JPanel {
     private BufferedImage img;
     private PaintSettings settings;
 
     public DrawPanel(PaintSettings settings) {
-        img = new BufferedImage(250, 250, BufferedImage.TYPE_3BYTE_BGR);
-        var g = img.createGraphics();
         this.settings = settings;
+
+        NewImage(250, 250);
         // TODO temp
         settings.CurrentPen = new TestPen(settings, img.createGraphics());
 
-        g.setColor(settings.backgroundColor);
-        g.fillRect(0, 0, img.getWidth(), img.getHeight());
-
+        // TODO add links of events to pen
         addMouseMotionListener(new MouseMotionListener() {
             @Override
             public void mouseDragged(MouseEvent e) {
@@ -69,5 +70,39 @@ public class DrawPanel extends JPanel {
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.drawImage(img, 0, 0, null);
+    }
+
+    public int LoadImage(File imgFile) {
+        if (imgFile.exists()) {
+            try {
+                img = ImageIO.read(imgFile); // TODO: other formats
+                settings.CurrentPen.SetGraphics(img.createGraphics());
+                this.repaint();
+                return 0;
+            } catch (IOException e) {
+                return 1;
+            }
+        }
+        return 1;
+    }
+    public int SaveImage(File imgFile) {
+        try {
+            ImageIO.write(img, "png", imgFile); // TODO: other formats
+            return 0;
+        } catch (IOException e) {
+            return 1;
+        }
+    }
+    public void NewImage(int width, int height) {
+        img = new BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR);
+        var g = img.createGraphics();
+        g.setColor(settings.backgroundColor);
+        g.fillRect(0, 0, img.getWidth(), img.getHeight());
+
+        if (settings.CurrentPen != null) {
+            settings.CurrentPen.SetGraphics(g);
+        }
+
+        this.repaint();
     }
 }
